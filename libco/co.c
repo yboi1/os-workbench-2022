30e40af82da1bc37e1863fd28a8ebc2bdeefdc74
tracer-nju <tracer@nju.edu.cn>
2024-02-16 17:55:36 +0800

    Linux boyi 6.5.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Sep 2023 22:55:13 +0000 x86_64 GNU/Linux  17:55:36 up  6:48,  1 user,  load average: 1.72, 1.64, 1.54

--- libco/co.c
+++ libco/co.c
@@ -1,15 +1,119 @@
 #include "co.h"
 #include <stdlib.h>
+#include <setjmp.h>
+#include <stdint.h>
 
-struct co {
+#define STACK_SIZE 4096
+#define MAX_NUM 26
+
+enum co_status{
+  co_NEW = 1,
+  co_RUNNING,
+  co_WAITING,
+  co_DEAD,
 };
 
+struct co {
+  char *name;
+  void(*func)(void *);
+  void *arg;
+
+  // struct co* next;    // 维护一个循环列表
+  enum co_status status;
+  struct co*     waiter;
+  jmp_buf        context;
+  uint8_t        stack[STACK_SIZE];         // stack_switch_call (x86.h)  实现上下文的切换
+
+
+};
+
+// 维护一个链表表示目前存有的协程
+typedef struct ListNode{
+  struct co val;
+  struct ListNode* next;
+
+  int num;
+}List;
+
+List* coList;
+
+
+// 创建一个新的协程   只是创建，不会立即执行任务
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+  if(coList->num > MAX_NUM){
+    printf("List size is too more\n");
+    return NULL;
+  }
+  struct co* New = (struct co*)malloc(sizeof(struct co));
+  New->name = name;
+  New->func = func;
+  New->arg = arg;
+  New->status = co_NEW;
+
+  List* tmp = coList;
+  while(tmp->next != NULL){
+    tmp=tmp->next;
+  }  
+  tmp->next = New;
+
+  return New;
 }
 
+// 等待--直到协程执行完毕   并将执行完任务的协程释放
 void co_wait(struct co *co) {
+  co->status = co_RUNNING;
+  co->func(co->arg);
+  co->status = co_DEAD;
+
+  if(!strcmp(coList->val.name,co->name)){
+    coList=coList->next;
+  }
+  else{
+    List* tmp = coList;
+    while(!strcmp(tmp->next->val.name, co->name)){
+      tmp=tmp->next;
+    }  
+    tmp->next = tmp->next->next;
+  }
+
+  free(co);
+  //  将协程释放并移除列表
 }
 
+
+//  int setjmp(jmp_buf env);    返回值为 longjmp中的参数 val
+//  void longjmp(jmp_buf env, int val);   无返回值
+
+// 将资源转让给协程
 void co_yield() {
+/*
+  首先先将当前执行的协程状态保存下来， 然后调用一个当前可以执行协程， 恢复其状态， 实现调换
+*/
+
+  // 找到当前执行的协程
+  List* tmp = coList;
+  while(tmp->next!=NULL && tmp->val.status!=co_RUNNING){
+    tmp = tmp->next;
+  }
+  
+
+
+  if(tmp->val.status==co_RUNNING){
+    tmp->val.status = co_wait;
+
+    List* find_p = coList;
+    // 找到可以切换的协程
+    while(find_p->next!=NULL && find_p->val.status!=co_DEAD && find_p->val.status!=co_RUNNING){
+      find_p = find_p->next;
+    }
+
+    tmp->val.waiter = find_p;
+    find_p->val.status=co_RUNNING;
+    
+    setjmp(tmp->val.context);
+
+    longjmp(find_p->val.context,  1);
+    
+  }
+
 }

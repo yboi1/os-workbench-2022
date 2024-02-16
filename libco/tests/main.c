f9df98ddf0ff123cabd275aa94fe0c3a5a7b4c91
tracer-nju <tracer@nju.edu.cn>
2024-02-16 18:19:41 +0800

    Linux boyi 6.5.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Sep 2023 22:55:13 +0000 x86_64 GNU/Linux  18:19:41 up  7:13,  1 user,  load average: 1.96, 1.34, 1.35

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -28,7 +28,7 @@ static void work(void *arg) {
 }
 
 static void test_1() {
-
+    printf("...\n");
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
     printf("co_start Ok...\n");
